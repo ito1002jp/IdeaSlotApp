1422679571f7a0fd080fef7c4ffdc28bc99d025a
ideaSlot2018 <ideaslotapp2018@gmail.com>
2018-07-22 17:34:14 +0900

    origin source

---
+++ IdeaSlotApp/ContentsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ContentsViewController.swift
+//  IdeaSlotApp
+//
+//  Created by yuta akazawa on 2018/07/22.
+//  Copyright © 2018年 yuta akazawa. All rights reserved.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController ,UIWebViewDelegate{
+    
+    var webView : UIWebView!
+    var siteUrl : String!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
--- IdeaSlotApp/ViewController.swift
+++ IdeaSlotApp/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import PageMenu
 
 class ViewController: UIViewController {
-
+    
+    // インスタンス配列
+    var controllerArray : [UIViewController] = []
+    var pageMenu : CAPSPageMenu?
+    
+    // サイト情報
+    let siteInfo:[Dictionary<String,String>] = [
+        ["title":"ヤフー！知恵袋","url":"http://chiebukuro.yahoo.co.jp/"],
+        ["title":"教えて！goo","url":"http://oshiete.goo.ne.jp/"],
+        ["title":"OKWAVE","url":"http://okwave.jp/"],
+        ["title":"発言小町","url":"http://komachi.yomiuri.co.jp/"],
+        ["title":"BIGLOBEなんでも相談室","url":"http://soudan.biglobe.ne.jp/sp/"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for site in siteInfo {
+            
+            let controller:ContentsViewController = ContentsViewController(nibName: "ContentsViewController", bundle: nil)
+            
+            controller.title = site["title"]!
+            controller.siteUrl = site["url"]!
+            
+            controller.webView = UIWebView(frame : self.view.bounds)
+            controller.webView.delegate = controller
+            controller.view.addSubview(controller.webView)
+            let req = URLRequest(url: URL(string:controller.siteUrl!)!)
+            controller.webView.loadRequest(req)
+            controllerArray.append(controller)
+            
+        }
+        
+        // Customize menu (Optional)
+        let parameters: [CAPSPageMenuOption] = [
+            .scrollMenuBackgroundColor(UIColor.white),
+            .viewBackgroundColor(UIColor.white),
+            .bottomMenuHairlineColor(UIColor.blue),
+            .selectionIndicatorColor(UIColor.red),
+            .menuItemFont(UIFont(name: "HelveticaNeue", size: 14.0)!),
+            .centerMenuItems(true),
+            .menuItemWidthBasedOnTitleTextWidth(true),
+            .menuMargin(16),
+            .selectedMenuItemLabelColor(UIColor.black),
+            .unselectedMenuItemLabelColor(UIColor.gray)
+            
+        ]
+        
+        // Initialize scroll menu
+        
+        let rect = CGRect(origin: CGPoint(x: 0,y :20), size: CGSize(width: self.view.frame.width, height: self.view.frame.height))
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: rect, pageMenuOptions: parameters)
+        
+        self.addChildViewController(pageMenu!)
+        self.view.addSubview(pageMenu!.view)
+        
+        pageMenu!.didMove(toParentViewController: self)
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
