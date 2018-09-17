71a9ce2dcc4b5034825af6f61db4069de3eba636
ideaSlot2018 <ideaslotapp2018@gmail.com>
2018-09-17 14:20:35 +0900

    plus button 'green'

--- IdeaSlotApp/Classes/ViewController/ParentWordsListViewController.swift
+++ IdeaSlotApp/Classes/ViewController/ParentWordsListViewController.swift
@@ -30,8 +30,6 @@ class ParentWordsListViewController: UIViewController {
     }
     
     func setPlusButton(){
-        let plusButtonImage1 = UIImage(named: "icons8-plus-32")
-        let plusButtonImage2 = UIImage(named: "icons8-plus-40")
         let plusButtonImage3 = UIImage(named: "icons8-plus-48")
         let plusButton = UIButton()
         
